cf9cbc84df61f9c793f02bac5dff91812f1d5327
Raul Lima <raul.lima@globalhitss.com.br>
2019-07-02 09:14:32 -0300

    create pagination in controller order

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -4,7 +4,7 @@ class OrderController {
   async index(req, res) {
     const response = await axios.get("/v2/orders");
     const { orders } = response.data;
-    res.json(orders);
+    res.json(paginate(orders, req.query.page_size, req.query.page_number));
   }
 
   async show(req, res) {
@@ -13,5 +13,8 @@ class OrderController {
     res.json(response.data);
   }
 }
-
+function paginate(array, page_size, page_number) {
+  --page_number;
+  return array.slice(page_number * page_size, (page_number + 1) * page_size);
+}
 module.exports = new OrderController();
